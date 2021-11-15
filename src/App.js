b38afcfb60ba9b26ab20585bd121f047dc45a5ad
David Duarte <jesusdavidduartejimenez@gmail.com>
2021-11-15 18:07:43 -0500

    new app 🚀

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import Todo from './components/Todo';
-
 function App() {
   return (
     <div>
       <h1>My todos</h1>
-      <Todo text="Learn React" />
-      <Todo text="Master React" />
-      <Todo text="Explore the full React course" />
     </div>
   );
 }
